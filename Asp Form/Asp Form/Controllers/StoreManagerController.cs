467157080f7652a0af64a05be431025e3ff2dbfc
evilkyro1965 <evilkyro1965@yahoo.com>
2015-05-12 00:50:56 +0700

    2

---
+++ Asp Form/Asp Form/Controllers/StoreManagerController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcMusicStore.Models;
+
+namespace Asp_Form.Controllers
+{
+    public class StoreManagerController : Controller
+    {
+        private MusicStoreDB db = new MusicStoreDB();
+
+        // GET: StoreManager
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: /StoreManager/Create
+        public ActionResult Create()
+        {
+            ViewBag.ArtistId = new SelectList(db.Artists, "ArtistId", "Name");
+            ViewBag.GenreId = new SelectList(db.Genres, "GenreId", "Name");
+            return View();
+        }
+
+        // POST: /StoreManager/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "AlbumId,GenreId,ArtistId,Title,Price,AlbumArtUrl")] Album album)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Albums.Add(album);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.ArtistId = new SelectList(db.Artists, "ArtistId", "Name", album.ArtistId);
+            ViewBag.GenreId = new SelectList(db.Genres, "GenreId", "Name", album.GenreId);
+            return View(album);
+        }
+
+    }
+}
---
+++ Asp Models/Asp Models/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcMusicStore.Models;
+
+namespace Asp_Models.Controllers
+{
+    public class HomeController : Controller
+    {
+        private MusicStoreDB db = new MusicStoreDB();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            Artist artist = db.Artists.Find(1);
+            ViewBag.Message = "About artist :"+artist.Name;
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
