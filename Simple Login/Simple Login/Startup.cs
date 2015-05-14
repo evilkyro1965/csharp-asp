211877e2c65d52b08e04a13e86f1d9facf4c543e
evilkyro1965 <evilkyro1965@yahoo.com>
2015-05-14 23:51:40 +0700

    login

---
+++ Simple Login/Simple Login/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Simple_Login.Startup))]
+namespace Simple_Login
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
